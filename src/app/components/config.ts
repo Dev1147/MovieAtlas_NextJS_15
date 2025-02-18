c382cf321d7d2324408290a7dc31b6cc87bd87ac
dev1147 <ehdrms940@gmail.com>
2025-02-18 23:16:05 +0900

    깃 대소문자 구분 지정

---
+++ src/app/components/config.ts
@@ -0,0 +1,7 @@
+export const USER_SERVER = '/api/users';
+
+export const API_URI = "https://api.themoviedb.org/3/";
+
+export const  IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
+
+//URL 상수로 지속적인 사영하기 위해 만들기
